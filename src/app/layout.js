8ae188b5e02ac3721da047976cf2645b2fdaa439
Gowdham2004 <gowdhammurugesh24@gmail.com>
2023-09-26 09:16:28 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "GDSC SJIT GCCP Leaderboard",
+  type: "website",
+  description: "This is the leaderboard for the GDSC SJIT GCCP Jams.",
+  keywords: ["gccp", "jams", "bit", "gdscbit"],
+  robots: "index,follow"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="/favicon.ico" />
+      <head>
+        
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        
+      </body>
+    </html>
+  )
+}
